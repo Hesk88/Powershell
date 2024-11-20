650a1c1625fed53a963a9db5a7836c6679cb3932
Hesk88 <160360364+Hesk88@users.noreply.github.com>
2024-11-20 16:33:47 +0000

    Add files via upload

---
+++ Clippy.ps1
@@ -0,0 +1,11 @@
+$username = [Environment]::UserName
+
+Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
+Write-Host " __                            "               
+Write-Host "/  \    I see you are trying to use powershell, $username, you magnificient person   "
+Write-Host "|  |     "
+Write-Host "@  @ /   "
+Write-Host "|| ||      "
+Write-Host "|| || "
+Write-Host "|\_/|  "   
+Write-Host "\___/      "
